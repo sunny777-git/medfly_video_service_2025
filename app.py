d7ae0a9647b8b9535068d4a2bf05e702922727da
sunnykumar <sunnykumar@sunny-MacBook-Pro.local>
2025-12-10 17:19:47 +0530

    debug removed

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def uploaded(filename):
     return jsonify({"url": local_url})
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
